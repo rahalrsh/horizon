60465b0e4724f2bd7f6810cd7be4624b1ad480d1
Rahal Ranasinghe <rahalrsh@gmail.com>
2025-03-03 21:31:57 -0500

    allowed hosts

--- horizon_core/settings_prod.py
+++ horizon_core/settings_prod.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['64.23.211.103']
 
 
 # Application definition
